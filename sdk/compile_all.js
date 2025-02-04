add5cbc1db5cae671f5c52a56695c00bc2ef93cb
gtrxAC <gtrxac2@gmail.com>
2025-02-04 17:25:44 +0200

    Improve stdout display of build script

--- sdk/compile_all.js
+++ sdk/compile_all.js
@@ -4,20 +4,38 @@ const targets = require('../build.json');
 const compileScript = (process.platform == 'win32') ? "sdk\\compile.bat" : "sdk/compile.sh";
 const classpathJoiner = (process.platform == 'win32') ? ";" : ":";
 
-targets.forEach(target => {
-  process.env.JAR_NAME = target.name;
-  process.env.DEFINES = "-D" + target.defines.join(" -D");
-  process.env.BOOTCLASSPATH = target.bootclasspath.join(classpathJoiner);
-  process.env.EXCLUDES = (target.excludes || []).join(" ");
-  
-  console.log(`${"_".repeat(80)}\n`)
-  console.log(` Compiling: ${target.name}`)
-  console.log(`${"_".repeat(80)}\n`)
-  try {
-    console.log(cp.execSync(compileScript).toString());
+const compileTarget = (target) => {
+  return new Promise((resolve, reject) => {
+    process.env.JAR_NAME = target.name;
+    process.env.DEFINES = "-D" + target.defines.join(" -D");
+    process.env.BOOTCLASSPATH = target.bootclasspath.join(classpathJoiner);
+    process.env.EXCLUDES = (target.excludes || []).join(" ");
+
+    console.log(`${"_".repeat(80)}\n`)
+    console.log(` Compiling: ${target.name}`)
+    console.log(`${"_".repeat(80)}\n`)
+
+    const compileProcess = cp.spawn(compileScript, [], { stdio: 'inherit' });
+
+    compileProcess.on('close', (code) => {
+      if (code !== 0) {
+        console.log("Compilation failed");
+        reject(new Error("Compilation failed"));
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const compileAll = async () => {
+  for (const target of targets) {
+    try {
+      await compileTarget(target);
+    } catch (error) {
+      process.exit(1);
+    }
   }
-  catch (e) {
-    console.log("Compilation failed");
-    process.exit(1);
-  }
-})
+};
+
+compileAll();
