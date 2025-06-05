7f5768dc71487fcf61c6c78fbabea1635fe4ef3c
gtrxAC <59177874+gtrxAC@users.noreply.github.com>
2025-06-06 01:07:16 +0300

    Shared RMS for custom theme (untested)
    For future theme editor app

--- src/com/gtrxac/discord/ThemeSaveDialog.java
+++ src/com/gtrxac/discord/ThemeSaveDialog.java
@@ -31,7 +31,11 @@ public class ThemeSaveDialog extends Dialog implements Strings {
                 // yes -> set theme to custom and save this theme's data
                 RecordStore rms = null;
                 try {
-                    rms = RecordStore.openRecordStore("theme", true);
+                     RecordStore.deleteRecordStore("theme");
+                }
+                catch (Exception e) {}
+                try {
+                    rms = RecordStore.openRecordStore("theme", true, RecordStore.AUTHMODE_ANY, true);
                     Util.setOrAddRecord(rms, 1, Util.stringToBytes(App.channelView.pendingTheme.build()));
                 }
                 catch (Exception e) {
