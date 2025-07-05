2cb82b17382874a2a574e97127ea0aab04cf8138
gtrxAC <gtrxac2@gmail.com>
2025-07-05 23:35:58 +0300

    proxy: add s40v2 to recommended versions

--- proxy/index.js
+++ proxy/index.js
@@ -303,7 +303,8 @@ function generateUploadToken(token) {
 
 const versionDownloadLinks = {
     midp2: `<p>5.0 for Nokia S40 and Symbian <br/> <a href="/discord_midp2.jad">JAD</a> - <a href="/discord_midp2.jar">JAR</a></p>`,
-    nokia_128px: `<p>5.0 for Nokia S40 (128x128, 128x160) <br/> <a href="/discord_nokia_128px.jad">JAD</a> - <a href="/discord_nokia_128px.jar">JAR</a></p>`,
+    nokia_128px: `<p>5.0 for Nokia S40v3+ (128x160) <br/> <a href="/discord_nokia_128px.jad">JAD</a> - <a href="/discord_nokia_128px.jar">JAR</a></p>`,
+    s40v2: `<p>5.0 for Nokia S40v2 <br/> <a href="/discord_s40v2.jad">JAD</a> - <a href="/discord_s40v2.jar">JAR</a></p>`,
     midp2_alt: `<p>5.0 for other MIDP2 devices <br/> <a href="/discord_midp2_alt.jad">JAD</a> - <a href="/discord_midp2_alt.jar">JAR</a></p>`,
     blackberry: `<p>5.0 for BlackBerry <br/> <a href="/discord_blackberry.jad">JAD</a> - <a href="/discord_blackberry.jar">JAR</a></p>`,
     samsung: `<p>5.0 for Samsung <br/> <a href="/discord_samsung.jad">JAD</a> - <a href="/discord_samsung.jar">JAR</a></p>`,
@@ -346,7 +347,10 @@ function getRecommendedVersions(req) {
         if (midp2) return ['lg'];
         return ["lg", "_6310i", "midp1"];
     }
-    if (/^nokia(168|2220|23[23]|26|27[26]|28|310|3110|315|322|350|507|514|520|602|6030|60[678]|610|6111|6125|6136|61[567]|623[05]|6255|68|707|72[67]|736|880\d\/|c1|c2-00)/g.test(ua)) {
+    if (/^nokia(2855|315|322|507|514|602|6030|60[67]|6085|610|615[25]|6170|623[05]|6255|68|72[67]|736|880\d\/)/g.test(ua)) {
+        return ["s40v2", "midp2_alt"];
+    }
+    if (/^nokia(168|2220|23[23]|26|27[26]|2865|310|3110|350|520|608[56]|6111|6125|6136|6151|616|707|c1|c2-00)/g.test(ua)) {
         return ["nokia_128px", "midp2_alt"];
     }
     if (midp2) {
