d64b665cf0977ac836e03ff979be18d7593da8ac
gtrxAC <gtrxac2@gmail.com>
2025-07-27 13:06:36 +0300

    Fix blank screen after reconnect on J2ME Loader (untested)

--- src/com/gtrxac/discord/ReconnectDialog.java
+++ src/com/gtrxac/discord/ReconnectDialog.java
@@ -12,6 +12,10 @@ public class ReconnectDialog extends Dialog implements CommandListener, Strings 
         super(Locale.get(RECONNECT_FORM_TITLE), "");
         setCommandListener(this);
         lastScreen = App.disp.getCurrent();
+        // ifdef J2ME_LOADER
+        // On J2ME Loader, the current screen may be null if the gateway disconnect occurred when the app was in the background
+        if (lastScreen == null) lastScreen = MainMenu.get(false);
+        // endif
 
         StringBuffer sb = new StringBuffer(Locale.get(Settings.autoReConnect ? AUTO_RECONNECT_FAILED : RECONNECT_FORM_TEXT));
         if (message != null && message.length() > 0) {
