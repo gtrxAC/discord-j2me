2d140263c74395aa3c3978fa077807d72012c55e
gtrxAC <gtrxac2@gmail.com>
2026-03-30 14:39:49 +0300

    Bump version (5.3)

--- src/com/gtrxac/discord/App.java
+++ src/com/gtrxac/discord/App.java
@@ -8,8 +8,8 @@ import java.util.*;
 import cc.nnproject.json.*;
 
 public class App extends MIDlet implements Strings {
-	public static final int VERSION_CODE = 34;
-	public static final String VERSION_NAME = "5.3.0 pre1";
+	public static final int VERSION_CODE = 35;
+	public static final String VERSION_NAME = "5.3.0";
 
 	// Should match the app's jar file name (used by auto update system)
 	public static final String VERSION_VARIANT =
