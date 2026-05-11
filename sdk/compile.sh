9191613bdfd5f10f743440ad1f60937e94494d98
gtrxAC <gtrxac2@gmail.com>
2026-05-11 19:51:23 +0300

    Show JAR size change when compiling

--- sdk/compile.sh
+++ sdk/compile.sh
@@ -41,6 +41,10 @@ ${JAVA_HOME}/bin/jar cvf bin/in.jar -C classes . -C build/res . >> sdk/log.txt
 [[ ${MODCON} == 1 ]] && ${JAVA_HOME}/bin/jar uvf bin/in.jar -C lib/bouncycastle . >> sdk/log.txt
 ${JAVA_HOME}/bin/jar uvfm bin/in.jar build/manifest.mf >> sdk/log.txt
 
+# Get size of previous files
+PREV_ZIP_SIZE=$((wc -c "bin/${JAR_NAME}.jar" 2> /dev/null | grep -Eo "^[0-9]+") || echo 0)
+PREV_UNZIP_SIZE=$((unzip -l bin/${JAR_NAME}.jar 2> /dev/null | grep -E "^\\s+([0-9]+).*?files$" | grep -Eo "^\\s+[0-9]+" | grep -Eo "[0-9]+") || echo 0)
+
 # Preverify and obfuscate (ProGuard)
 # Note: ProGuard 6.0.3 is the last version to run under JDK 6, as of writing, the latest version can run under JDK 8
 echo "Verifying"
@@ -49,4 +53,13 @@ rm bin/in.jar
 mv bin/out.jar "bin/${JAR_NAME}.jar"
 
 # Show output JAR file size
-wc -c "bin/${JAR_NAME}.jar"
+ZIP_SIZE=$((wc -c "bin/${JAR_NAME}.jar" 2> /dev/null | grep -Eo "^[0-9]+") || echo 0)
+ZIP_DIFF=$[ $ZIP_SIZE - $PREV_ZIP_SIZE ]
+[[ $ZIP_DIFF -gt 0 ]] && ZIP_DIFF="+$ZIP_DIFF"
+
+UNZIP_SIZE=$((unzip -l bin/${JAR_NAME}.jar 2> /dev/null | grep -E "^\\s+([0-9]+).*?files$" | grep -Eo "^\\s+[0-9]+" | grep -Eo "[0-9]+") || echo 0)
+UNZIP_DIFF=$[ $UNZIP_SIZE - $PREV_UNZIP_SIZE ]
+[[ $UNZIP_DIFF -gt 0 ]] && UNZIP_DIFF="+$UNZIP_DIFF"
+
+echo $ZIP_SIZE "($ZIP_DIFF)" "bin/${JAR_NAME}.jar"
+echo $UNZIP_SIZE "($UNZIP_DIFF)" uncompressed
